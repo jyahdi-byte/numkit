81e5b7138b5c261a01c4a551db0dcaeed3d559bd
jyahdi-byte <jyahdi@gmail.com>
2026-08-26 21:16:41 -0700

    Improve update_cell.hpp

--- include/update_cell.hpp
+++ include/update_cell.hpp
@@ -7,14 +7,15 @@
 #define NUMKIT_HD
 #endif 
 
+NUMKIT_HD double neighbor_sum(const double* temps, const double* faces_k, int r, int c, int rows, int cols){
+    double k_up = faces_k[(r * cols + c) * 4 + 0]; double k_down = faces_k[(r * cols + c) * 4 + 1];
+    double k_right = faces_k[(r * cols + c) * 4 + 2]; double k_left = faces_k[(r * cols + c) * 4 + 3];
+    return (k_up * temps[(r + 1) * cols + c] + k_down * temps[(r - 1) * cols + c]
+      + k_right * temps[r * cols + (c + 1)] + k_left * temps[r * cols + (c - 1)]);
+}
+
 NUMKIT_HD double update_cell(const double* temps, const double* faces_k, const double* total_ks, const unsigned char* active, int r, int c, int rows, int cols){
-    if (active[r * cols + c]){
-        double k_up = faces_k[(r * cols + c) * 4 + 0];
-        double k_down = faces_k[(r * cols + c) * 4 + 1];
-        double k_right = faces_k[(r * cols + c) * 4 + 2];
-        double k_left = faces_k[(r * cols + c) * 4 + 3];
-        return (k_up * temps[(r + 1) * cols + c] + k_down * temps[(r - 1) * cols + c] + k_right * temps[r * cols + (c + 1)] + k_left * temps[r * cols + (c - 1)]) / total_ks[r * cols + c];
-    }
+    if (active[r * cols + c]){return neighbor_sum(temps, faces_k, r, c, rows, cols) / total_ks[r * cols + c];}
     else{return temps[r * cols + c];}
 }
  
