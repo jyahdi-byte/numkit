d492898dea485a69a8a8e24d0aa0c04b8052a15d
jyahdi-byte <jyahdi@gmail.com>
2026-08-27 01:50:45 -0700

    Add Grid3D overload of jacobi_solve

--- include/jacobi.hpp
+++ include/jacobi.hpp
@@ -43,4 +43,44 @@ int jacobi_solve(Grid& g, double tol, int max_iter){
     return max_iter;
 }
 
+int jacobi_solve(Grid3D& g, double tol, int max_iter){
+    std::vector<double> cur(g.getRows() * g.getCols() * g.getDepth()); std::vector<double> nxt(g.getRows() * g.getCols() * g.getDepth());
+    for (int k = 0; k < g.getDepth(); k++){
+        for (int i = 0; i < g.getRows(); i++){
+            for (int j = 0; j < g.getCols(); j++){
+                cur[(k * g.getRows() * g.getCols()) + i * g.getCols() + j] = g.at(i,j, k);
+                nxt[(k * g.getRows() * g.getCols()) + i * g.getCols() + j] = g.at(i,j, k);
+            }
+        }
+    }
+
+    for (int n = 0; n < max_iter; n++){
+        double maxChange = 0;
+        double change = 0;
+        for (int k = 1; k < g.getDepth() - 1; k++){
+            for (int i = 1; i < g.getRows() - 1; i++){ 
+                for (int j = 1; j < g.getCols() - 1; j++){ 
+                    if (g.getType(i,j, k) == INTERIOR){
+                        nxt[(k * g.getRows() * g.getCols()) + i * g.getCols() + j] = update_cell(cur.data(), g.getFacesKPtr(), g.getTotalKPtr(), g.getActivePtr(), i, j, k, g.getRows(), g.getCols(), g.getDepth());
+                        change = std::abs(nxt[(k * g.getRows() * g.getCols()) + i * g.getCols() + j] - cur[(k * g.getRows() * g.getCols()) + i * g.getCols() + j]);
+                        if (change >= maxChange){maxChange = change;}
+                    }
+                }
+            }
+        }
+        std::swap(cur, nxt);
+        if (maxChange <= tol){
+            for (int k = 0; k < g.getDepth(); k++){
+                for (int i = 0; i < g.getRows(); i++){
+                    for (int j = 0; j < g.getCols(); j++){g.at(i,j,k) = cur[(k * g.getRows() * g.getCols()) + i * g.getCols() + j];}}}
+            return n+1;
+        }
+    }
+
+    for (int k = 0; k < g.getDepth(); k++){
+        for (int i = 0; i < g.getRows(); i++){
+                for (int j = 0; j < g.getCols(); j++){g.at(i,j,k) = cur[(k * g.getRows() * g.getCols()) + i * g.getCols() + j];}}}
+    return max_iter;
+}
+
 #endif
